4b583dc1b9ff3d3db7a8eed5763f313bc9bd18b6
Imre Vari <imrevari2@gmail.com>
2023-11-27 09:40:35 +0100

    node server added with mocked data

---
+++ src/server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+const router = require('./router')
+app.use('/api', router)
+
+
+app.listen(3001, () => console.log('Server Started'))
